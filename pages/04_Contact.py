378f5d4eef24a1fdb1c487b5fac644f05e9d69d5
Satyajeet Padhi <satyajeet.padhi@ansys.com>
2023-05-16 11:34:22 +0530

    Code cleanup

--- pages/04_Contact.py
+++ pages/04_Contact.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.set_page_config(layout="centered", page_icon="🌡️", page_title="PCB Thermal Analyzer")
 st.title('📧Contact', anchor='contact_page')
 
-st.markdown('Report issues here')
+report_issues = '[Report issues here](https://github.com/ansys-satyajeet/pcb_thermal_analyzer/issues)'
+st.markdown(report_issues, unsafe_allow_html=True)
+
